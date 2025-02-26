775609ec893a9751e908e57e867295633824e378
dlozon <dlozon@gkcorp.com>
2025-02-26 11:56:29 -0500

    Implemented a potential fix for the elevator - Removed unused functions from Robot.java - Released Elevator command from command factory -- Splitting it into its own command class allows for more precise ending control - Added setPosition() to Elevator, set drive command to use the setpoint provided by that function

--- src/main/java/frc/robot/commands/SetUpperChassisPose.java
+++ src/main/java/frc/robot/commands/SetUpperChassisPose.java
@@ -1,21 +1,18 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.UpperChassisConstants.UpperChassisPose;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Pivot;
 
 /** Moves the pivot to a safe position, then drives the elevator 
  *  to the correct height, then moves the pivot to its destination and ends */
-public class SetUpperChassisPose extends SequentialCommandGroup{
+public class SetUpperChassisPose extends SequentialCommandGroup {
     public SetUpperChassisPose(Elevator elevator, Pivot pivot, UpperChassisPose pose){
         addCommands(
             pivot.goToPosition(UpperChassisPose.L1_SCORE),
-            elevator.elevateToPosition(pose),
-            new WaitCommand(2),
+            new SetElevatorPosition(elevator, pose),
             pivot.goToPosition(pose)
-
         );
     }
 }
